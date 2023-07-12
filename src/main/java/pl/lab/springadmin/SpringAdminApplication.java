40263bb3a27ee1d8649b41bc26aa28e836fa5702
bartek <bartoszbud@gmail.com>
2023-07-12 21:35:30 +0200

    initial commit

---
+++ src/main/java/pl/lab/springadmin/SpringAdminApplication.java
@@ -0,0 +1,22 @@
+package pl.lab.springadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableAutoConfiguration
+@EnableScheduling
+@Configuration
+
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+}
