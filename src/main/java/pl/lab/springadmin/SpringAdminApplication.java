53dfe784a49c035bc7bae5850e87ec90ea5bb165
bartek <bartoszbud@gmail.com>
2024-02-04 15:51:33 +0100

    logback configuration

--- src/main/java/pl/lab/springadmin/SpringAdminApplication.java
+++ src/main/java/pl/lab/springadmin/SpringAdminApplication.java
@@ -1,9 +1,17 @@
 package pl.lab.springadmin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
+@EnableAdminServer
+@EnableAutoConfiguration
+@EnableScheduling
+@Configuration
+
 public class SpringAdminApplication {
 
 	public static void main(String[] args) {
